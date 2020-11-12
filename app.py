aecde60aec7186474df416c7def57783c73a2f74
Nyco <nyxnyschd@posteo.de>
2020-11-12 12:27:05 +0100

    updated suchfunktion, app.py, suche.html

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, render_template
-
-from backendKrams import datastuff
+import pandas as pd
+import spacy
+from spacy.lang.en import English
+from backendKrams import suchfunktion
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def start():
+    # short_desc_eng = pd.read_csv('cpv_short_desc_eng.csv', delimiter=',')
+    # shorty = pd.DataFrame(short_desc_eng)
+    # short_desc = shorty['short_desc_eng'].tolist()
     if request.method == 'POST':
         suchwort = request.form['input']
-        results = datastuff.all_values_containing_substring(suchwort)
+        results = suchfunktion.all_values_containing_substring(suchwort)
         return render_template('/suche.html', len=len(results), results=results)
 
     else:
---
+++ suchfunktion.py
@@ -0,0 +1,41 @@
+import re
+import pandas as pd
+import spacy
+from spacy.lang.en import English
+from spacy.tokenizer import Tokenizer
+
+nlp = English()
+tokenizer = Tokenizer(nlp.vocab)
+
+short_desc_eng = pd.read_csv('cpv_short_desc_eng.csv', delimiter=',')
+shorty = pd.DataFrame(short_desc_eng)
+nlp = spacy.load("en_core_web_sm")
+
+cleaned = []
+for i in range(len(shorty)):
+    text = shorty['short_desc_eng'][i]
+    lemma = [tok.lemma_ for tok in nlp(text)]
+    no_punct = [tok for tok in lemma if re.match('\w+', tok)]
+    no_zeichen = [tok for tok in no_punct if not re.match('\. \+ \* \( \) \[ \] \- \$ \|', tok)]
+    no_numbers = [tok for tok in no_zeichen if not re.match('\d+', tok)]
+    no_whitespaces = [tok for tok in no_numbers if not re.match('\s+', tok)]
+    filtered_sentence = []
+    for word1 in no_whitespaces:
+        lexeme = nlp.vocab[word1]
+        if not lexeme.is_stop:
+            filtered_sentence.append(word1)
+    cleaned.append(filtered_sentence)
+
+
+def all_values_containing_substring(substring):
+    list = cleaned
+    gotIt = []
+    for i, s in enumerate(list):
+        if substring in s:
+            gotIt.append(shorty['short_desc_eng'][i])
+            gotIt.append("_____________________________________________________________________________")
+    # for index in range(len(gotIt)):
+    #     print(gotIt[index])
+    return gotIt
+
+#all_values_containing_substring("home")

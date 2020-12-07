f8acc1200d631d6d049bf27f3846d24888a3c93f
Dein Name <du@example.com>
2020-12-07 19:49:06 +0100

    Review Code

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template
+from backend import suchfunktion
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def start():
+    if request.method == 'POST':
+        suchwort = request.form['input']
+        results = suchfunktion.all_values_containing_substring(suchwort)
+        return render_template('suche.html', len=len(results), results=results)
+
+    else:
+
+        return render_template('suche.html')
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def more():
+#     if request.method == 'GET':
+#         ergebnisse = request.form['submit']
+#         showmores = suchfunktion.values_containing_substring(ergebnisse)
+#         return render_template('suche.html', len=len(showmores), showmores=showmores)
+#
+#     else:
+#
+#         return render_template('suche.html')
+#
+#
+# def contact():
+#     if request.method == 'POST':
+#         if request.form['submit_button'] == 'Do Something':
+#             ergebnisse = request.form['submit']
+#             showmores = suchfunktion.values_containing_substring(ergebnisse)
+#             return render_template('suche.html', len=len(showmores), showmores=showmores)
+#         else:
+#
+#             return render_template('suche.html')
+
+
+if __name__ == '__main__':
+    app.run()

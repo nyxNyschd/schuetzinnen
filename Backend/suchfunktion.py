b312f10e0c09d1da60c59bf0c9fd2e1b6de03496
buhali10 <buhali10@gmail.com>
2021-02-05 17:41:14 +0100

    05.02.21

--- Backend/suchfunktion.py
+++ Backend/suchfunktion.py
@@ -1,22 +1,20 @@
 import pickle
 import re
-from turtle import pd
-
 import spacy
+import time
 from fuzzywuzzy import process
 from spacy.lang.en import English
-from utils import data_df
+from Backend.utils import long
 nlp = English()
 nlp = spacy.load("en_core_web_lg")
 
-
-def cleaning_query(substring):
+def substring_cleaning(substring):
     cleaned_query = []
     lemma = [tok.lemma_ for tok in nlp(substring)]
     no_punct = [tok for tok in lemma if re.match('\w+', tok)]
     no_zeichen = [tok for tok in no_punct if not re.match('\. \+ \* \( \) \[ \] \- \$ \|', tok)]
-    #no_numbers = [tok for tok in no_zeichen if not re.match('\d+', tok)]
-    no_whitespaces = [tok for tok in no_zeichen if not re.match('\s+', tok)]
+    no_numbers = [tok for tok in no_zeichen if not re.match('\d+', tok)]
+    no_whitespaces = [tok for tok in no_numbers if not re.match('\s+', tok)]
     for word in no_whitespaces:
         lexeme = nlp.vocab[word]
         if not lexeme.is_stop:
@@ -35,53 +33,69 @@ def fuzzy_logic(substring):
                 most_relevant_word = temp[0]
     return most_relevant_word
 
-def merge_index_sum(dict1, dict2):
+def merge_dict_summ(dict1, dict2):
+    # print({k: index[query[0]].get(k, 0) + index[query[1]].get(k, 0) for k in set(index[query[0]]) | set(index[query[1]])}
     return {k: dict1.get(k, 0) + dict2.get(k, 0) for k in set(dict1) | set(dict2)}
 
+def preprocess_query(query_words):
+    return [w if w in LOOKUP_TABLE.keys() else fuzzy_logic(w) for w in query_words]
+
 def main_search(query):
-    cleaned_query = cleaning_query(query)
-    print(cleaned_query)
+#    cleaned_query = substring_cleaning(query)
+#    print(cleaned_query)
     result = {}
-    for word in cleaned_query:
+    gotIt = []
+
+    for word in query:
         if word in LOOKUP_TABLE.keys():
             #print(word, LOOKUP_TABLE[word])
             # for i in LOOKUP_TABLE[word].keys():
             #     print(i, long['long_desc_eng'][i])
-            result = merge_index_sum(result, LOOKUP_TABLE[word])
-        else:
-            fuzzy = fuzzy_logic(word)
-            print(fuzzy)
-            result = merge_index_sum(result, LOOKUP_TABLE[fuzzy])
+            result = merge_dict_summ(result, LOOKUP_TABLE[word])
+        #elif try_fuzzy is True:
+        #    fuzzy = fuzzy_logic(word)
+        #    print(fuzzy)
+        #    result = merge_dict_summ(result, LOOKUP_TABLE[fuzzy])
     result = sorted(result, key=result.get, reverse=True)
-    print(result)
+ #   print(result)
+    #for i in result:
+    #    gotIt.append(long['long_desc_eng'][i])
+ #   for i in range(len(gotIt)):
+ #       print(gotIt[i])
+    return [long['long_desc_eng'][i] for i in result]
 
-    gotIt = data_df.iloc[result, 0:13]
+def similar_search(query):
+    print(query)
+#    cleaned_query = substring_cleaning(query)
+#    print(cleaned_query)
 
-    # for i in result:
-    #     gotIt.append(data_df['long_desc_eng'][i])
-    #     gotIt.append('•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••')
-    # for i in range(len(gotIt)):
-    #     print(gotIt[i])
-    #return gotIt
-    print(gotIt)
+    similar_words = []
+    for word in query:
+        if word in WORD2VEC_TABLE:
+            similar_words += WORD2VEC_TABLE[word]
 
+    print(similar_words)
+
+ #   return main_search(' '.join(similar_words))
+    return main_search(similar_words)
 #wenn ihr mit Frontend arbeiten möchtet:
-# if __name__ == 'Backend.suchfunktion':
-#     infile1 = open('Backend/tokens', 'rb')
-#     CLEANED = pickle.load(infile1)
-#     infile1.close()
-#     infile2 = open('Backend/lookup_table', 'rb')
-#     LOOKUP_TABLE = pickle.load(infile2)
-#     infile2.close()
-
-
-
-#wenn ihr mit Backend arbeiten möchtet:
-if __name__ == '__main__':
-    infile1 = open('tokens', 'rb')
+if __name__ == 'Backend.suchfunktion':
+    infile1 = open('Backend/tokens', 'rb')
     CLEANED = pickle.load(infile1)
     infile1.close()
-    infile2 = open('lookup_table', 'rb')
+    infile2 = open('Backend/lookup_table', 'rb')
     LOOKUP_TABLE = pickle.load(infile2)
     infile2.close()
-    main_search('1343400')
+    # infile3 = open('Backend/word2vec_table', 'rb')
+    # WORD2VEC_TABLE = pickle.load(infile3)
+    # infile3.close()
+
+#wenn ihr mit Backend arbeiten möchtet:
+# if __name__ == '__main__':
+#     infile1 = open('tokens', 'rb')
+#     CLEANED = pickle.load(infile1)
+#     infile1.close()
+#     infile2 = open('lookup_table', 'rb')
+#     LOOKUP_TABLE = pickle.load(infile2)
+#     infile2.close()
+#     main_search('permanent daily')
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from Backend import suchfunktion
+import time
+from Backend.suchfunktion import substring_cleaning, preprocess_query
 
 app = Flask(__name__)
 app.debug = True
@@ -8,8 +10,19 @@ app.debug = True
 def start():
     if request.method == 'POST':
         suchwort = request.form['input']
+        suchwort = substring_cleaning(suchwort)
+        suchwort = preprocess_query(suchwort)
+        start_time=time.time()
         results = suchfunktion.main_search(suchwort)
-        return render_template('suche.html', len=len(results), results=results)
+        time_1=time.time()-start_time
+        print("Time 1: ", time_1)
+        similar_results = suchfunktion.similar_search(suchwort)
+        time_2=time.time()-time_1
+        print("Time 2: ", time_2)
+        similar_results = [text for text in similar_results if text not in results]
+        time_3=time.time()-time_2
+        print("Time 3: ", time_3)
+        return render_template('suche.html', len=len(results), results=results, similar_results=similar_results)
     else:
         return render_template('suche.html')
 

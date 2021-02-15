311d6a2074cfd1df428b7c0a73818e7290be9149
buhali10 <buhali10@gmail.com>
2021-02-15 13:51:37 +0100

    Endergebnis

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
 from flask import Flask, request, render_template
 from backend import suchfunktion
+import time
+from backend.suchfunktion import substring_cleaning, preprocess_query
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/', methods=['GET', 'POST'])
 def start():
     if request.method == 'POST':
         suchwort = request.form['input']
-<<<<<<< HEAD
-        results = suchfunktion.suche_substring(suchwort)
-=======
-        results = suchfunktion.all_values_containing_substring(suchwort)
->>>>>>> f8acc12... Review Code
-        return render_template('suche.html', len=len(results), results=results)
-
+        suchwort = substring_cleaning(suchwort)
+        suchwort = preprocess_query(suchwort)
+        print(suchwort)
+        # start_time = time.time()
+        results = suchfunktion.main_search(suchwort)
+        # time_1 = time.time() - start_time
+        # print("Time 1: ", time_1)
+        similar_word = suchfunktion.similar_word(suchwort)
+        print(similar_word)
+        # time_2 = time.time() - time_1
+        # print("Time 2: ", time_2)
+        similar_results = suchfunktion.similar_search(similar_word)
+        # time_3 = time.time() - time_2
+        # print("Time 3: ", time_3)
+        return render_template('suche.html', len=len(results), results=results, similar_results=similar_results,
+                               similar_word=similar_word)
     else:
-
         return render_template('suche.html')
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def more():
-#     if request.method == 'GET':
-#         ergebnisse = request.form['submit']
-#         showmores = suchfunktion.values_containing_substring(ergebnisse)
-#         return render_template('suche.html', len=len(showmores), showmores=showmores)
-#
-#     else:
-#
-#         return render_template('suche.html')
-#
-#
-# def contact():
-#     if request.method == 'POST':
-#         if request.form['submit_button'] == 'Do Something':
-#             ergebnisse = request.form['submit']
-#             showmores = suchfunktion.values_containing_substring(ergebnisse)
-#             return render_template('suche.html', len=len(showmores), showmores=showmores)
-#         else:
-#
-#             return render_template('suche.html')
 
 
 if __name__ == '__main__':
--- backend/suchfunktion.py
+++ backend/suchfunktion.py
@@ -1,157 +1,74 @@
-import math
+import pickle
 import re
-from collections import Counter
-import pandas as pd
 import spacy
-from spacy.lang.en import English
-from spacy.tokenizer import Tokenizer
-<<<<<<< HEAD
-from fuzzy_logic import fuzzy_logic
-
-=======
-from fuzzywuzzy import process
->>>>>>> f8acc12... Review Code
-
-nlp = English()
-tokenizer = Tokenizer(nlp.vocab)
-long_desc_eng = pd.read_csv('long_staging_xml_2020.csv', delimiter=',')
-shorty = pd.DataFrame(long_desc_eng)
+from fuzzywuzzy import fuzz, process
+from backend.utils import get_corpus
 nlp = spacy.load("en_core_web_lg")
 
-cleaned = []
-for i in range(len(shorty)):
-    text = shorty['long_desc_eng'][i]
-    lemma = [tok.lemma_ for tok in nlp(text)]
+def substring_cleaning(substring):
+    cleaned_query = []
+    lemma = [tok.lemma_ for tok in nlp(substring)]
     no_punct = [tok for tok in lemma if re.match('\w+', tok)]
     no_zeichen = [tok for tok in no_punct if not re.match('\. \+ \* \( \) \[ \] \- \$ \|', tok)]
-    no_whitespaces = [tok for tok in no_zeichen if not re.match('\s+', tok)]
-    filtered_sentence = []
+    no_numbers = [tok for tok in no_zeichen if not re.match('\d+', tok)]
+    no_whitespaces = [tok for tok in no_numbers if not re.match('\s+', tok)]
     for word in no_whitespaces:
         lexeme = nlp.vocab[word]
         if not lexeme.is_stop:
-            filtered_sentence.append(word)
-    cleaned.append(filtered_sentence)
+            cleaned_query.append(word)
+    return cleaned_query
 
 
-# print(cleaned)
-
-def substring_cleaning(substring):
-    doc1 = nlp(substring)
-    for token in doc1:
-        lemma = token.lemma_
-        no_punct = lemma if re.match('\w+', substring) else lemma
-        no_zeichen = no_punct if not re.match('[\,\+\*\(\)\|\[\]\?\!\/\=\{\}\#\&\;\:\_]', substring) else no_punct
-        cleaned_substring = no_zeichen if not re.match('\s+', substring) else no_zeichen
-        return cleaned_substring
-
-
-<<<<<<< HEAD
-=======
 def fuzzy_logic(substring):
     highest_value = 0
     most_relevant_word = ' '
-    for i in range(len(cleaned)):
-        Ratios = process.extract(substring, cleaned[i])
-        for index in range(len(Ratios)):
-            if highest_value < (Ratios[index][1]) and len(Ratios[index][0]) > 2:
-                temp = process.extractOne(substring, cleaned[i])
-                highest_value = temp[1]
-                most_relevant_word = temp[0]
+    Ratios = process.extract(substring, CLEANED, limit=10, scorer=fuzz.ratio)
+    # print(Ratios)
+    for index in range(len(Ratios)):
+        if len(Ratios[index][0]) > 2:
+            most_relevant_word = Ratios[index][0]
+            break
     return most_relevant_word
 
 
->>>>>>> f8acc12... Review Code
-def list_ranking(list):
-    ranked_list = []
-    corpus_tf = []
-
-    for text in list:
-        tf_text = Counter(text)
-        tf_text = {i: tf_text[i] / float(len(text)) for i in tf_text}
-        corpus_tf.append(tf_text)
-
-    unique_words = set()
-    for text in list:
-        unique_words = set(unique_words).union(set(text))
-
-    word_idf = {}
-    for word in unique_words:
-        word_idf[word] = math.log10(len(list) / sum([1.0 for i in cleaned if word in i]))
-
-    tfidf = {}
-    for i, text_tf in enumerate(corpus_tf):
-        for word in text_tf.keys():
-            if word not in tfidf:
-                tfidf[word] = {}
-            tfidf[word][i] = round(text_tf[word] * word_idf[word], 4)
-
-    for index, valuesList in tfidf.items():
-        for values in valuesList.items():
-            sort_orders = sorted(valuesList.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-            tfidf[index] = sort_orders.copy()
-
-    # for index, values in tfidf.items():
-    #     for j in range(len(values)):
-    #         if substring in index:
-    #             #print(values[j][0])
-    #             ranked_list.append(list[tfidf[substring][j][0]])
-    return tfidf
+def merge_dict_summ(dict1, dict2):
+    return {k: dict1.get(k, 0) + dict2.get(k, 0) for k in set(dict1) | set(dict2)}
 
 
-<<<<<<< HEAD
-def suche_substring(substring):
-    result = all_values_containing_substring(substring)
-    if len(result) == 0:
-        print("fuzzy")
-        fuzzy = fuzzy_logic(substring, cleaned)
-        result = all_values_containing_substring(fuzzy)
-    return result
+def preprocess_query(query_words):
+    return [w if w in LOOKUP_TABLE.keys() else fuzzy_logic(w) for w in query_words]
 
+def main_search(query):
+    result = {}
+    for word in query:
+        if word in LOOKUP_TABLE.keys():
+            result = merge_dict_summ(result, LOOKUP_TABLE[word])
+    result = sorted(result, key=result.get, reverse=True)
+    long = get_corpus()
+    texts = []
+    for i in result:
+        if long['long_desc_eng'][i] not in texts:
+            texts.append(long['long_desc_eng'][i])
+    return texts[:20]
 
-def all_values_containing_substring(substring):
-    cleaned_searched_word = substring_cleaning(substring)
-=======
-def all_values_containing_substring(substring):
-    fuzzy = fuzzy_logic(substring)
-    cleaned_searched_word = substring_cleaning(fuzzy)
->>>>>>> f8acc12... Review Code
-    print("Das relevanteste Wort: " + cleaned_searched_word)
-    ranked_dict = list_ranking(cleaned)
-    gotIt = []
-    # for index, values in ranked_dict.items():
-    #     print(index, values)
-    for s, list in ranked_dict.items():
-        for j in range(len(list)):
-            if cleaned_searched_word in s:
-                gotIt.append(shorty['long_desc_eng'][list[j][0]])
-                gotIt.append("......................................................................................")
-<<<<<<< HEAD
-    return gotIt[:9]
-=======
-    for index in range(len(gotIt)):
-        return gotIt[:9]
->>>>>>> f8acc12... Review Code
+def similar_search(similar_words):
+    similar_words = similar_word(similar_words)
+    return main_search(similar_words)[:20]
 
+def similar_word(query):
+    similar_words = []
+    for word in query:
+        if word in WORD2VEC_TABLE:
+            similar_words += WORD2VEC_TABLE[word]
+    return similar_words
 
-# def values_containing_substring(substring):
-#     fuzzy = fuzzy_logic(substring)
-#     cleaned_searched_word = substring_cleaning(fuzzy)
-#     print("Das relevanteste Wort: " + cleaned_searched_word)
-#     ranked_dict = list_ranking(cleaned)
-#     gotIt = []
-#     # for index, values in ranked_dict.items():
-#     #     print(index, values)
-#     for s, list in ranked_dict.items():
-#         for j in range(len(list)):
-#             if cleaned_searched_word in s:
-#                 gotIt.append(shorty['long_desc_eng'][list[j][0]])
-#                 gotIt.append("......................................................................................")
-#     for index in range(len(gotIt)):
-#         return gotIt[9:]
-
-# all_values_containing_substring("limit")
-<<<<<<< HEAD
-#print(suche_substring("wark"))
-
-=======
->>>>>>> f8acc12... Review Code
+if __name__ == 'backend.suchfunktion':
+    infile1 = open('backend/tokens', 'rb')
+    CLEANED = pickle.load(infile1)
+    infile1.close()
+    infile2 = open('backend/lookup_table', 'rb')
+    LOOKUP_TABLE = pickle.load(infile2)
+    infile2.close()
+    infile3 = open('backend/word2vec_table', 'rb')
+    WORD2VEC_TABLE = pickle.load(infile3)
+    infile3.close()
